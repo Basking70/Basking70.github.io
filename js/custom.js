15a52f98f0e4c2d9a527ce521d9eef1527659889
Basking70 <navab@usc.edu>
2017-05-02 13:28:41 -0700

    added some pictures

--- js/custom.js
+++ js/custom.js
@@ -17,6 +17,7 @@ $(function(){
              "images/tm-bg-slide-5.jpg",
              "images/tm-bg-slide-6.jpg",
              "images/tm-bg-slide-7.jpg",
+             "images/tm-bg-slide-8.jpg",
              "images/tm-bg-slide-8.jpg"
 	 			], 	{duration: 3200, fade: 1300});
 		});
